03b0cbaa1e73b7f0e712b856d3686ed6636d13f0
maxisses <max_dargatz@hotmail.com>
2020-10-06 14:06:20 +0200

    new

--- deployment-scripts/productpage.sh
+++ deployment-scripts/productpage.sh
@@ -3,10 +3,10 @@
 export NAMESPACE=custom-tekton
 export DEPLOYMENT_NAME=productpage
 export CONTAINER_NAME=productpage
-export MANIFEST_FILE_OR_DIR=https://raw.githubusercontent.com/Javatar81/pipeline-examples/main/productpage.yaml
-export GIT_URL=https://github.com/istio/istio.git
-export GIT_SUBDIR=samples/bookinfo/src/$DEPLOYMENT_NAME/
-export GIT_REVISION=master
+export MANIFEST_FILE_OR_DIR=productpage.yaml
+export GIT_URL=https://github.com/Javatar81/pipeline-examples.git
+export GIT_SUBDIR=$DEPLOYMENT_NAME/
+export GIT_REVISION=main
 
 chmod +x start-pipe-run.sh
 
--- deployment-scripts/reviews.sh
+++ deployment-scripts/reviews.sh
@@ -5,7 +5,7 @@ export DEPLOYMENT_NAME=reviews
 export CONTAINER_NAME=review
 export MANIFEST_FILE_OR_DIR=https://raw.githubusercontent.com/Javatar81/pipeline-examples/main/reviews.yaml
 export GIT_URL=https://github.com/Javatar81/pipeline-examples.git
-export GIT_SUBDIR=reviews/
+export GIT_SUBDIR=$DEPLOYMENT_NAME/
 export GIT_REVISION=main
 
 chmod +x start-pipe-run.sh
