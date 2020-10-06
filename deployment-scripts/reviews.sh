03760cbd0fe1cc3ceecf51b968258778ce2865b2
maxisses <max_dargatz@hotmail.com>
2020-10-06 13:38:44 +0200

    cleanup

--- deployment-scripts/reviews.sh
+++ deployment-scripts/reviews.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
 export NAMESPACE=custom-tekton
-export DEPLOYMENT_NAME=details
-export CONTAINER_NAME=details
-export MANIFEST_FILE_OR_DIR=samples/bookinfo/platform/kube/bookinfo-details.yaml
-export GIT_URL=https://github.com/istio/istio.git
+export DEPLOYMENT_NAME=reviews
+export CONTAINER_NAME=review
+export MANIFEST_FILE_OR_DIR=https://raw.githubusercontent.com/Javatar81/pipeline-examples/main/reviews.yaml
+export GIT_URL=https://raw.githubusercontent.com/Javatar81/pipeline-examples.git
 export GIT_SUBDIR=samples/bookinfo/src/$DEPLOYMENT_NAME/
 
 chmod +x start-pipe-run.sh
-
-echo $DEPLOYMENT_NAME
 
 /bin/bash start-pipe-run.sh
